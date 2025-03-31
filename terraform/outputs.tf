a1b0c9f2c9a61888e06f9b34cd28cfa315c4e22b
anushas9901 <anusha.pinkey3@gmail.com>
2025-03-31 15:33:17 -0400

    Added variables.tf and outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,10 @@
  
+output "eks_cluster_name" {
+  description = "EKS Cluster Name"
+  value       = aws_eks_cluster.genome_eks.name
+}
+
+output "rds_endpoint" {
+  description = "PostgreSQL RDS endpoint"
+  value       = aws_rds_instance.genome_db.endpoint
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,15 @@
  
+variable "aws_region" {
+  description = "AWS region for deployment"
+  default     = "us-east-1"
+}
+
+variable "eks_cluster_name" {
+  description = "Name of the EKS cluster"
+  default     = "genome-cluster"
+}
+
+variable "db_instance_type" {
+  description = "Database instance type"
+  default     = "db.t3.micro"
+}
